66db4f72286c410d6d12fa685292e11dab09562c
SwamynathanProfessional <swamynathan.bommaganty@gmail.com>
2021-09-14 16:58:50 +0530

    hibye

--- src/UI/Home/Home.js
+++ src/UI/Home/Home.js
@@ -13,81 +13,148 @@ import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import {ProfileScreen} from './ProfileScreen';
+import Orientation from 'react-native-orientation';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
 export const Home = () => {
-  var array = [
-    {title: 'account-circle'},
-    {title: 'bell'},
-    {title: 'calendar-month'},
-    {title: 'format-page-break'},
-    {title: 'book-open-page-variant'},
-  ];
-
-  const [idno, setidno] = useState(0);
+  const [profile, setProfile] = useState(true);
+  const [notification, setNotification] = useState(false);
+  const [calendar, setCalendar] = useState(false);
+  const [catalog, setCatalog] = useState(false);
+  const [random, setRandom] = useState(false);
   const [isPortrait, setIsPortrait] = useState();
-  let content;
+
   useEffect(() => {
-    if (windowHeight > windowWidth) {
+    Orientation.unlockAllOrientations();
+    const initial = Orientation.getInitialOrientation();
+    if (initial === 'PORTRAIT') {
       setIsPortrait(true);
     } else {
       setIsPortrait(false);
     }
-  }, [windowWidth, windowHeight]);
 
-  if (idno === 0) {
-    content = <ProfileScreen />;
-  } else if (idno === 1) {
-    content = (
-      <View>
-        <Text style={styles.screenText}>Notification screen</Text>
-      </View>
-    );
-  } else if (idno === 2) {
-    content = (
-      <View>
-        <Text style={styles.screenText}>Calender screen</Text>
-      </View>
-    );
-  } else if (idno === 3) {
-    content = (
-      <View>
-        <Text style={styles.screenText}>Catalog screen</Text>
-      </View>
-    );
-  } else if (idno === 4) {
-    content = (
-      <View>
-        <Text style={styles.screenText}>Book screen</Text>
-      </View>
-    );
-  }
+    const _orientationDidChange = orientation => {
+      if (orientation === 'LANDSCAPE') {
+        setIsPortrait(false);
+      } else {
+        setIsPortrait(true);
+      }
+    };
+    Orientation.addOrientationListener(_orientationDidChange);
+
+    return () => Orientation.removeOrientationListener(_orientationDidChange);
+  }, []);
+
+  const profileHandler = () => {
+    setProfile(true);
+    setNotification(false);
+    setCalendar(false);
+    setCatalog(false);
+    setRandom(false);
+  };
+  const notificationHandler = () => {
+    setProfile(false);
+    setNotification(true);
+    setCalendar(false);
+    setCatalog(false);
+    setRandom(false);
+  };
+
+  const calendarHandler = () => {
+    setProfile(false);
+    setNotification(false);
+    setCalendar(true);
+    setCatalog(false);
+    setRandom(false);
+  };
+
+  const catalogHandler = () => {
+    setProfile(false);
+    setNotification(false);
+    setCalendar(false);
+    setCatalog(true);
+    setRandom(false);
+  };
+
+  const randomHandler = () => {
+    setProfile(false);
+    setNotification(false);
+    setCalendar(false);
+    setCatalog(false);
+    setRandom(true);
+  };
 
   const BottomTab = () => {
     return (
       <>
-        {array.map((item, index) => (
-          <View style={styles.box}>
-            <TouchableOpacity
-              onPress={() => {
-                setidno(index);
-              }}>
-              <MaterialCommunityIcons
-                name={item.title}
-                size={index === idno ? 35 : 24}
-                color={index === idno ? 'white' : 'rgba(255, 255, 255, 0.5)'}
-              />
-            </TouchableOpacity>
-          </View>
-        ))}
+        <TouchableOpacity style={styles.iconContainer} onPress={profileHandler}>
+          <FontAwesome
+            name="user-circle"
+            size={profile ? 30 : 24}
+            color={profile ? 'white' : 'rgba(255, 255, 255, 0.5)'}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.iconContainer}
+          onPress={notificationHandler}>
+          <Ionicons
+            name="notifications"
+            size={notification ? 30 : 24}
+            color={notification ? 'white' : 'rgba(255, 255, 255, 0.5)'}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.iconContainer}
+          onPress={calendarHandler}>
+          <FontAwesome
+            name="calendar-o"
+            size={calendar ? 30 : 24}
+            color={calendar ? 'white' : 'rgba(255, 255, 255, 0.5)'}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.iconContainer} onPress={catalogHandler}>
+          <MaterialCommunityIcons
+            name="format-page-break"
+            size={catalog ? 30 : 24}
+            color={catalog ? 'white' : 'rgba(255, 255, 255, 0.5)'}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.iconContainer} onPress={randomHandler}>
+          <Octicons
+            name="book"
+            size={random ? 30 : 24}
+            color={random ? 'white' : 'rgba(255, 255, 255, 0.5)'}
+          />
+        </TouchableOpacity>
       </>
     );
   };
   return (
     <View style={styles.container}>
-      {content}
+      {profile && <ProfileScreen />}
+
+      {notification && (
+        <View>
+          <Text style={styles.screenText}>Notification screen</Text>
+        </View>
+      )}
+      {calendar && (
+        <View>
+          <Text style={styles.screenText}>Calendar screen</Text>
+        </View>
+      )}
+      {catalog && (
+        <View>
+          <Text style={styles.screenText}>Catalog screen</Text>
+        </View>
+      )}
+      {random && (
+        <View>
+          <Text style={styles.screenText}>Random screen</Text>
+        </View>
+      )}
       <View style={isPortrait ? styles.BottomTabConatiner : styles.sideTab}>
         {BottomTab()}
       </View>
